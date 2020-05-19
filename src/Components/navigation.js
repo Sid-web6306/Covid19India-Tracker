379d6c4bbf75f421ab36b837b9c695890d7b9dcc
Siddhant Jain <siddhantjain15298@gmail.com>
2020-05-19 14:22:27 +0530

    changes

--- src/Components/navigation.js
+++ src/Components/navigation.js
@@ -5,7 +5,7 @@ const Navigation = ({onSearch,searchBar}) =>{
         <div className='flex justify-between'>
             <Logo />
             <h2 className ='tc'>Covid19-India Tracker</h2>
-            <input onChange={onSearch} className="h2 w50 pa2" type = "search" placeholder="Search state" />
+            <input onChange={onSearch} className="h2 w50 pa2" type = "search" placeholder="Search State" />
         </div>
     );
 }
