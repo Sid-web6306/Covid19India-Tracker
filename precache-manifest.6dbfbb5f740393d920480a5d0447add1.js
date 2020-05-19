2d6bec5e3be0a6678a8e810a22815242c0812a9b
Siddhant Jain <siddhantjain15298@gmail.com>
2020-05-19 14:07:39 +0530

    Updates

---
+++ precache-manifest.6dbfbb5f740393d920480a5d0447add1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ead1f982171e6c4cda7b71ef5ec24034",
+    "url": "/Covid19India-Tracker/index.html"
+  },
+  {
+    "revision": "5bff6f62606d19181562",
+    "url": "/Covid19India-Tracker/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cfcfa369f37acd08c453",
+    "url": "/Covid19India-Tracker/static/css/main.53784db0.chunk.css"
+  },
+  {
+    "revision": "5bff6f62606d19181562",
+    "url": "/Covid19India-Tracker/static/js/2.b1911d48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Covid19India-Tracker/static/js/2.b1911d48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfcfa369f37acd08c453",
+    "url": "/Covid19India-Tracker/static/js/main.4c15cfc1.chunk.js"
+  },
+  {
+    "revision": "a6bc5c96888f9cac1f0c",
+    "url": "/Covid19India-Tracker/static/js/runtime-main.796c5c03.js"
+  }
+]);
